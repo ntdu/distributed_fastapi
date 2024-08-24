a0fb84e9f3fc1b936d17b46dd2ea87240f53279d
daniel <ntdu198@gmail.com>
2024-08-24 16:48:19 +0700

    update: remove unneccessary import

--- services/backend-api-service/src/routers/file_processing/s3_processing.py
+++ services/backend-api-service/src/routers/file_processing/s3_processing.py
@@ -12,7 +12,7 @@ from fastapi import APIRouter, UploadFile, Form
 from typing import List, Optional
 
 from src.settings import get_settings
-from src.aws import upload_to_s3, generate_presigned_url, generate_presigned_url_with_signer
+from src.aws import upload_to_s3, generate_presigned_url_with_signer
 
 settings = get_settings()
 
@@ -46,12 +46,11 @@ async def stream_file(
             )
 
         upload_to_s3(file.file, settings.S3_BUCKET_NAME, s3_image_key)
+
         return custom_response(
             status_code=status.HTTP_201_CREATED,
             data={ 
                 "session_file_path": f'{settings.S3_STREAM_PREFIX}/{user_id}/{session_id}/{datetime_prefix}_{hash_name}',
-                # "check": generate_presigned_url(settings.S3_BUCKET_NAME, s3_image_key)
-                # "url": f'{settings.CLOUD_FRONT_URL}/{s3_image_key}',
                 "url": generate_presigned_url_with_signer(f'{settings.CLOUD_FRONT_URL}/{s3_image_key}')
                 },
             )
